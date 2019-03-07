a92428a5fb91989e7fe233f1fccdfda68f41f357
dpayton23 <darien.payton71@gmail.com>
2019-03-07 17:02:49 -0500

    udated methods and fixed bugs

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 class Dropdown {
   constructor(element) {
-    
+    console.log(element)
     // Assign this.element to the dropdown element
     this.element = element;
     
@@ -8,7 +8,7 @@ class Dropdown {
     this.button = this.element.querySelector(".dropdown-button");
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelectorAll(".dropdown-content");
+    this.content = this.element.querySelector(".dropdown-content");
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => this.toggleContent());
@@ -17,7 +17,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('dropdown-content');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
