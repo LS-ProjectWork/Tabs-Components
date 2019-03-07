eab8db56a5550643b587d82eae92b3a2c01f661c
dpayton23 <darien.payton71@gmail.com>
2019-03-07 15:44:35 -0500

    updated Dropdown method

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -17,7 +17,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-content');
   }
 }
 
