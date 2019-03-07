d780257f9b4a4a46a310d3de7803c8df47b15740
dpayton23 <darien.payton71@gmail.com>
2019-03-07 17:20:18 -0500

    updated link selector

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,13 +2,13 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tab-items[data-tab = '${this.element.dataset.tab}']`);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
@@ -59,4 +59,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll(".tabs-link").forEach(link, () => new TabLink(link));
+links = document.querySelectorAll(".tabs-link").forEach(link => new TabLink(link));
